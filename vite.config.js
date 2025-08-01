e3947c9ffaf064e0990c02d746620c906351badd
AliShinde <alishinde21@gmail.com>
2025-08-01 13:08:06 +0530

    Commit with gh-pages deployment

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import dotenv from "dotenv"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Aliraza_Portfolio.github.io/",
+  base: "/aliraza_portfolio/",
   define: {
     'process.env.VITE_EMAILJS_API': JSON.stringify(process.env.VITE_EMAILJS_API),
     'process.env.VITE_EMAILJS_SERVICE': JSON.stringify(process.env.VITE_EMAILJS_SERVICE),
